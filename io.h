79935259765ae202850a71518d7ce7bfb8337975
Varun Gujarathi <v.gujarathi777@gmail.com>
2018-10-16 19:08:03 +0530

    Minor change

--- io.h
+++ io.h
@@ -1,3 +1,12 @@
+/******************************************************
+ |		#########   #########	#########	          |
+ |		    #       #       #   #		              |
+ | 		    #       #       #   #		              |
+ |		    #       #########	######		          |
+ |		    #       #  #        #		              |
+ |		    #       #     #     #		              |
+ |		    #       #       #   #		              |
+ *******************************************************/
 #include <stdlib.h>
 #include <stdbool.h>
 #include <string.h>
@@ -6,196 +15,23 @@
 #include <avr/pgmspace.h>
 #include <avr/io.h>
 #include <avr/interrupt.h>
-#define HIGH 1
-#define LOW 0
 #ifndef F_CPU
 #define F_CPU 16000000UL   //SET CPU CLOCK
 #define FOSC 16000000
 #endif
 #include <util/delay.h>
-          
-//pinMode
+
+const uint8_t OUTPUT=1,INPUT=0;
+const uint8_t HIGH=1,LOW=0;
+const uint8_t RISING=2,FALLING=3,CHANGE=4;      
+
+//pinMapping
 int c[60]={0,1,4,5,5,3,3,4,5,6,4,5,6,7,1,0,1,0,3,2,1,0,6,7,0,1,2,3,4,5,6,7,7,6,5,4,3,2,1,0,7,2,1,0,7,6,5,4,3,2,1,0,3,2,1,0};
 
 char d[60]={'e','e','e','e','g','e','h','h','h','h','b','b','b','b','j','j','h','h','d','d','a','a','a','a','a','a','a','a','c','c','c','c','c','c','c','c','d','g','g','g','l','l','l','l','l','l','l','l','b','b','b','b'};
 	
-void pinMode(unsigned int i,int tipe)
-{
-	int l;
-  if(tipe==0)
-        { switch(d[i])
-         {
-	 
-	          case'b':
-	                  DDRB|=(0<<c[i]);
-			           break;
-	          case'c':
-	                 DDRC|=(0<<c[i]);
-			          break;
-	          case'd':
-	                 DDRD|=(0<<c[i]);
-			          break;
-				//case'h':
-				  //  DDRH|(0<<c[i]);
-		      case'e':
-	                  DDRE|=(0<<c[i]);
-			           break;
 
-			     case'g':
-	                  DDRG|=(0<<c[i]);
-			           break;
-			 case'h':
-	                  DDRH|=(0<<c[i]);
-					  		           break;
-		     
-			     case'j':
-	                  DDRJ|=(0<<c[i]);
-			           break;
-				     case'l':
-	                  DDRL|=(0<<c[i]);
-			           break;	        		   		   	  	  		 		 			 
-	     }  }
-	else{          switch(d[i])
-         {
-	 
-	          case'b':
-	                  DDRB|=(1<<c[i]);
-			           break;
-	          case'c':
-	                 DDRC|=(1<<c[i]);
-			          break;
-	          case'd':
-	                 DDRD|=(1<<c[i]);
-			          break;
-					 
-				    
-		      case'e':
-	                  DDRE|=(1<<c[i]);
-			           break;
-
-			     case'g':
-	                  DDRG|=(1<<c[i]);
-			           break;
-			 case'h':
-	                  DDRH|=(1<<c[i]);
-			           break;
-			     case'j':
-	                  DDRJ|=(1<<c[i]);
-			           break;
-				     case'l':
-	                  DDRL|=(1<<c[i]);
-			           break;	  		 		 			 
-	     }  
-}
-		 }
-void digitalWrite (int i ,int tdipe)		 
-
-{
-   if(tdipe==0)
-        { switch(d[i])
-         {
-	 
-	          case'b':
-	                  PORTB|=(0<<c[i]);
-			           break;
-	          case'c':
-	                 PORTC|=(0<<c[i]);
-			          break;
-	          case'd':
-	                 PORTD|=(0<<c[i]);
-			          break;
-				
-		      case'e':
-	                  PORTE|=(0<<c[i]);
-			           break;
-
-			     case'g':
-	                  PORTG|=(0<<c[i]);
-			           break;
-			 case'h':
-	                  PORTH|=(0<<c[i]);
-			           break;
-		     
-			     case'j':
-	                  PORTJ|=(0<<c[i]);
-			           break;
-				     case'l':
-	                  PORTL|=(0<<c[i]);
-			           break;	        		   		   	  	  		 		 			 
-	     }  }
-	else{          switch(d[i])
-         {
-	 
-	          case'b':
-	                 PORTB|=(1<<c[i]);
-			           break;
-	          case'c':
-	                 PORTC|=(1<<c[i]);
-			          break;
-	          case'd':
-	                 PORTD|=(1<<c[i]);
-			          break;
-				    
-		      case'e':
-	                  PORTE|=(1<<c[i]);
-			           break;
-
-			     case'g':
-	                  PORTG|=(1<<c[i]);
-			           break;
-			 case'h':
-	                  PORTH|=(1<<c[i]);
-			           break;
-		     /*case'i':
-	                  PORTI|=(1<<c[i]);
-			           break;*/
-			     case'j':
-	                  PORTJ|=(1<<c[i]);
-			           break;
-				     case'l':
-	                  PORTL|=(1<<c[i]);
-			           break;	  		 		 			 
-	     }  
-}
-		 }
-		 
-
-uint8_t digitalRead(int i)
-{  uint8_t z;
-	uint8_t x;
-   switch(d[i])
-         {
-	 
-	          case'b':
-	                  z=PINB&(1<<c[i]);
-			           break;
-	          case'c':
-	                  z=PINC&(1<<c[i]);
-			          break;
-	          case'd':
-	                  z=PIND&(1<<c[i]);
-			          break;
-				
-		      case'e':
-	                   z=PINE&(1<<c[i]);
-			           break;
-
-			     case'g':
-	                   z=PING&(1<<c[i]);
-			           break;
-			 case'h':
-	                  z=PINH&(1<<c[i]);
-			           break;
-		     
-			     case'j':
-	                   z=PINJ&(1<<c[i]);
-			           break;
-				     case'l':
-	                   z=PINL&(1<<c[i]);
-			           break;
-					   return z;	        		   		   	  	  		 		 			 
-	     }  }
-
+/**************************************************************************************************************************/
 
 //Function declaration
 void analogWrite(uint8_t ,uint8_t);
@@ -208,8 +44,6 @@ void attachIntterupt(int, void *,int);
 void (*cAllisr)(void); //function pointer used in ISR()
 void softwareInterrupt(void *);
 void (*uSerfun(void));
-
-
 void pinMode(uint8_t , uint8_t );
 static void turnOffPWM(uint8_t );
 void digitalWrite(uint8_t , uint8_t );
@@ -218,41 +52,192 @@ long unsigned int microsecondsToInches(long unsigned int);
 long unsigned int microsecondsToCentimeters(long unsigned int);
 void setup(void);
 void loop(void);
-//Function:
 
-/*void pinMode(uint8_t pIn, uint8_t mOde)
+/**************************************************************************************************************************/
+//Functions:
+
+void pinMode(uint8_t pInno,uint8_t mOde)
 {
-	uint8_t bit = digitalPinToBitMask(pIn);
-	uint8_t port = digitalPinToPort(pIn);
-	volatile uint8_t *reg, *out;
+	int l;
+  if(mOde==0)
+        { 
+        	switch(d[pInno])
+         	{
+	 
+	          case'b':
+	                  DDRB|=(0<<c[pInno]);
+			           break;
+	          case'c':
+	                 DDRC|=(0<<c[pInno]);
+			          break;
+	          case'd':
+	                 DDRD|=(0<<c[pInno]);
+			          break;
+				//case'h':
+				  //  DDRH|(0<<c[pInno]);
+		      case'e':
+	                  DDRE|=(0<<c[pInno]);
+			           break;
 
-	if (port == NOT_A_PIN) return;
+			     case'g':
+	                  DDRG|=(0<<c[pInno]);
+			           break;
+			 case'h':
+	                  DDRH|=(0<<c[pInno]);
+					  		           break;
+		     
+			     case'j':
+	                  DDRJ|=(0<<c[pInno]);
+			           break;
+				     case'l':
+	                  DDRL|=(0<<c[pInno]);
+			           break;	        		   		   	  	  		 		 			 
+	     }  }
+	else{          switch(d[pInno])
+         {
+	 
+	          case'b':
+	                  DDRB|=(1<<c[pInno]);
+			           break;
+	          case'c':
+	                 DDRC|=(1<<c[pInno]);
+			          break;
+	          case'd':
+	                 DDRD|=(1<<c[pInno]);
+			          break;
+					 
+				    
+		      case'e':
+	                  DDRE|=(1<<c[pInno]);
+			           break;
 
-	// JWS: can I let the optimizer do this?
-	reg = portModeRegister(port);
-	out = portOutputRegister(port);
-
-	if (mOde == INPUT) { 
-		uint8_t oldSREG = SREG;
-                cli();
-		*reg &= ~bit;
-		*out &= ~bit;
-		SREG = oldSREG;
-	} else if (mOde == INPUT_PULLUP) {
-		uint8_t oldSREG = SREG;
-                cli();
-		*reg &= ~bit;
-		*out |= bit;
-		SREG = oldSREG;
-	} else {
-		uint8_t oldSREG = SREG;
-                cli();
-		*reg |= bit;
-		SREG = oldSREG;
+			     case'g':
+	                  DDRG|=(1<<c[pInno]);
+			           break;
+			 case'h':
+	                  DDRH|=(1<<c[pInno]);
+			           break;
+			     case'j':
+	                  DDRJ|=(1<<c[pInno]);
+			           break;
+				     case'l':
+	                  DDRL|=(1<<c[pInno]);
+			           break;	  		 		 			 
+	     }  
 	}
 }
 
-static void turnOffPWM(uint8_t tImer)
+void digitalWrite (uint8_t pInno ,uint8_t mOde)		 
+
+{
+   if(mOde==0)
+        { switch(d[pInno])
+         {
+	 
+	          case'b':
+	                  PORTB|=(0<<c[pInno]);
+			           break;
+	          case'c':
+	                 PORTC|=(0<<c[pInno]);
+			          break;
+	          case'd':
+	                 PORTD|=(0<<c[pInno]);
+			          break;
+				
+		      case'e':
+	                  PORTE|=(0<<c[pInno]);
+			           break;
+
+			     case'g':
+	                  PORTG|=(0<<c[pInno]);
+			           break;
+			 case'h':
+	                  PORTH|=(0<<c[pInno]);
+			           break;
+		     
+			     case'j':
+	                  PORTJ|=(0<<c[pInno]);
+			           break;
+				     case'l':
+	                  PORTL|=(0<<c[pInno]);
+			           break;	        		   		   	  	  		 		 			 
+	     }  }
+	else{          switch(d[pInno])
+         {
+	 
+	          case'b':
+	                 PORTB|=(1<<c[pInno]);
+			           break;
+	          case'c':
+	                 PORTC|=(1<<c[pInno]);
+			          break;
+	          case'd':
+	                 PORTD|=(1<<c[pInno]);
+			          break;
+				    
+		      case'e':
+	                  PORTE|=(1<<c[pInno]);
+			           break;
+
+			     case'g':
+	                  PORTG|=(1<<c[pInno]);
+			           break;
+			 case'h':
+	                  PORTH|=(1<<c[pInno]);
+			           break;
+		     /*case'i':
+	                  PORTI|=(1<<c[pInno]);
+			           break;*/
+			     case'j':
+	                  PORTJ|=(1<<c[pInno]);
+			           break;
+				     case'l':
+	                  PORTL|=(1<<c[pInno]);
+			           break;	  		 		 			 
+	     }  
+}
+		 }
+		 
+
+uint8_t digitalRead(int pInno)
+{  
+	uint8_t z;				//not a good practice
+	uint8_t x;				//not a good practice
+   switch(d[pInno])
+         {
+	 
+	          case'b':
+	                  z=PINB&(1<<c[pInno]);
+			           break;
+	          case'c':
+	                  z=PINC&(1<<c[pInno]);
+			          break;
+	          case'd':
+	                  z=PIND&(1<<c[pInno]);
+			          break;
+				
+		      case'e':
+	                   z=PINE&(1<<c[pInno]);
+			           break;
+
+			     case'g':
+	                   z=PING&(1<<c[pInno]);
+			           break;
+			 case'h':
+	                  z=PINH&(1<<c[pInno]);
+			           break;
+		     
+			     case'j':
+	                   z=PINJ&(1<<c[pInno]);
+			           break;
+				     case'l':
+	                   z=PINL&(1<<c[pInno]);
+			           break;
+					   return z;	        		   		   	  	  		 		 			 
+	     }  
+}
+
+/*static void turnOffPWM(uint8_t tImer)
 {
 	switch (tImer)
 	{
@@ -314,201 +299,7 @@ static void turnOffPWM(uint8_t tImer)
 		#endif
 	}
 }
-
-void digitalWrite(uint8_t pIn, uint8_t vAl)
-{
-	uint8_t tImer = digitalPinToTimer(pIn);
-	uint8_t bit = digitalPinToBitMask(pIn);
-	uint8_t port = digitalPinToPort(pIn);
-	volatile uint8_t *out;
-
-	if (port == NOT_A_PIN) return;
-
-	// If the pin that support PWM output, we need to turn it off
-	// before doing a digital write.
-	if (tImer != NOT_ON_TIMER) turnOffPWM(tImer);
-
-	out = portOutputRegister(port);
-
-	uint8_t oldSREG = SREG;
-	cli();
-
-	if (vAl == LOW) {
-		*out &= ~bit;
-	} else {
-		*out |= bit;
-	}
-
-	SREG = oldSREG;
-}
-
-int digitalRead(uint8_t pIn)
-{
-	uint8_t tImer = digitalPinToTimer(pIn);
-	uint8_t bit = digitalPinToBitMask(pIn);
-	uint8_t port = digitalPinToPort(pIn);
-
-	if (port == NOT_A_PIN) return LOW;
-
-	// If the pin that support PWM output, we need to turn it off
-	// before getting a digital reading.
-	if (tImer != NOT_ON_TIMER) turnOffPWM(tImer);
-
-	if (*portInputRegister(port) & bit) return HIGH;
-	return LOW;
-}
-	
-	const uint8_t PROGMEM digital_pin_to_port_PGM[] = {
-	// PORTLIST		
-	// -------------------------------------------		
-	PE	, // PE 0 ** 0 ** USART0_RX	
-	PE	, // PE 1 ** 1 ** USART0_TX	
-	PE	, // PE 4 ** 2 ** PWM2	
-	PE	, // PE 5 ** 3 ** PWM3	
-	PG	, // PG 5 ** 4 ** PWM4	
-	PE	, // PE 3 ** 5 ** PWM5	
-	PH	, // PH 3 ** 6 ** PWM6	
-	PH	, // PH 4 ** 7 ** PWM7	
-	PH	, // PH 5 ** 8 ** PWM8	
-	PH	, // PH 6 ** 9 ** PWM9	
-	PB	, // PB 4 ** 10 ** PWM10	
-	PB	, // PB 5 ** 11 ** PWM11	
-	PB	, // PB 6 ** 12 ** PWM12	
-	PB	, // PB 7 ** 13 ** PWM13	
-	PJ	, // PJ 1 ** 14 ** USART3_TX	
-	PJ	, // PJ 0 ** 15 ** USART3_RX	
-	PH	, // PH 1 ** 16 ** USART2_TX	
-	PH	, // PH 0 ** 17 ** USART2_RX	
-	PD	, // PD 3 ** 18 ** USART1_TX	
-	PD	, // PD 2 ** 19 ** USART1_RX	
-	PD	, // PD 1 ** 20 ** I2C_SDA	
-	PD	, // PD 0 ** 21 ** I2C_SCL	
-	PA	, // PA 0 ** 22 ** D22	
-	PA	, // PA 1 ** 23 ** D23	
-	PA	, // PA 2 ** 24 ** D24	
-	PA	, // PA 3 ** 25 ** D25	
-	PA	, // PA 4 ** 26 ** D26	
-	PA	, // PA 5 ** 27 ** D27	
-	PA	, // PA 6 ** 28 ** D28	
-	PA	, // PA 7 ** 29 ** D29	
-	PC	, // PC 7 ** 30 ** D30	
-	PC	, // PC 6 ** 31 ** D31	
-	PC	, // PC 5 ** 32 ** D32	
-	PC	, // PC 4 ** 33 ** D33	
-	PC	, // PC 3 ** 34 ** D34	
-	PC	, // PC 2 ** 35 ** D35	
-	PC	, // PC 1 ** 36 ** D36	
-	PC	, // PC 0 ** 37 ** D37	
-	PD	, // PD 7 ** 38 ** D38	
-	PG	, // PG 2 ** 39 ** D39	
-	PG	, // PG 1 ** 40 ** D40	
-	PG	, // PG 0 ** 41 ** D41	
-	PL	, // PL 7 ** 42 ** D42	
-	PL	, // PL 6 ** 43 ** D43	
-	PL	, // PL 5 ** 44 ** D44	
-	PL	, // PL 4 ** 45 ** D45	
-	PL	, // PL 3 ** 46 ** D46	
-	PL	, // PL 2 ** 47 ** D47	
-	PL	, // PL 1 ** 48 ** D48	
-	PL	, // PL 0 ** 49 ** D49	
-	PB	, // PB 3 ** 50 ** SPI_MISO	
-	PB	, // PB 2 ** 51 ** SPI_MOSI	
-	PB	, // PB 1 ** 52 ** SPI_SCK	
-	PB	, // PB 0 ** 53 ** SPI_SS	
-	PF	, // PF 0 ** 54 ** A0	
-	PF	, // PF 1 ** 55 ** A1	
-	PF	, // PF 2 ** 56 ** A2	
-	PF	, // PF 3 ** 57 ** A3	
-	PF	, // PF 4 ** 58 ** A4	
-	PF	, // PF 5 ** 59 ** A5	
-	PF	, // PF 6 ** 60 ** A6	
-	PF	, // PF 7 ** 61 ** A7	
-	PK	, // PK 0 ** 62 ** A8	
-	PK	, // PK 1 ** 63 ** A9	
-	PK	, // PK 2 ** 64 ** A10	
-	PK	, // PK 3 ** 65 ** A11	
-	PK	, // PK 4 ** 66 ** A12	
-	PK	, // PK 5 ** 67 ** A13	
-	PK	, // PK 6 ** 68 ** A14	
-	PK	, // PK 7 ** 69 ** A15	
-};
-
-const uint8_t PROGMEM digital_pin_to_bit_mask_PGM[] = {
-	// PIN IN PORT		
-	// -------------------------------------------		
-	_BV( 0 )	, // PE 0 ** 0 ** USART0_RX	
-	_BV( 1 )	, // PE 1 ** 1 ** USART0_TX	
-	_BV( 4 )	, // PE 4 ** 2 ** PWM2	
-	_BV( 5 )	, // PE 5 ** 3 ** PWM3	
-	_BV( 5 )	, // PG 5 ** 4 ** PWM4	
-	_BV( 3 )	, // PE 3 ** 5 ** PWM5	
-	_BV( 3 )	, // PH 3 ** 6 ** PWM6	
-	_BV( 4 )	, // PH 4 ** 7 ** PWM7	
-	_BV( 5 )	, // PH 5 ** 8 ** PWM8	
-	_BV( 6 )	, // PH 6 ** 9 ** PWM9	
-	_BV( 4 )	, // PB 4 ** 10 ** PWM10	
-	_BV( 5 )	, // PB 5 ** 11 ** PWM11	
-	_BV( 6 )	, // PB 6 ** 12 ** PWM12	
-	_BV( 7 )	, // PB 7 ** 13 ** PWM13	
-	_BV( 1 )	, // PJ 1 ** 14 ** USART3_TX	
-	_BV( 0 )	, // PJ 0 ** 15 ** USART3_RX	
-	_BV( 1 )	, // PH 1 ** 16 ** USART2_TX	
-	_BV( 0 )	, // PH 0 ** 17 ** USART2_RX	
-	_BV( 3 )	, // PD 3 ** 18 ** USART1_TX	
-	_BV( 2 )	, // PD 2 ** 19 ** USART1_RX	
-	_BV( 1 )	, // PD 1 ** 20 ** I2C_SDA	
-	_BV( 0 )	, // PD 0 ** 21 ** I2C_SCL	
-	_BV( 0 )	, // PA 0 ** 22 ** D22	
-	_BV( 1 )	, // PA 1 ** 23 ** D23	
-	_BV( 2 )	, // PA 2 ** 24 ** D24	
-	_BV( 3 )	, // PA 3 ** 25 ** D25	
-	_BV( 4 )	, // PA 4 ** 26 ** D26	
-	_BV( 5 )	, // PA 5 ** 27 ** D27	
-	_BV( 6 )	, // PA 6 ** 28 ** D28	
-	_BV( 7 )	, // PA 7 ** 29 ** D29	
-	_BV( 7 )	, // PC 7 ** 30 ** D30	
-	_BV( 6 )	, // PC 6 ** 31 ** D31	
-	_BV( 5 )	, // PC 5 ** 32 ** D32	
-	_BV( 4 )	, // PC 4 ** 33 ** D33	
-	_BV( 3 )	, // PC 3 ** 34 ** D34	
-	_BV( 2 )	, // PC 2 ** 35 ** D35	
-	_BV( 1 )	, // PC 1 ** 36 ** D36	
-	_BV( 0 )	, // PC 0 ** 37 ** D37	
-	_BV( 7 )	, // PD 7 ** 38 ** D38	
-	_BV( 2 )	, // PG 2 ** 39 ** D39	
-	_BV( 1 )	, // PG 1 ** 40 ** D40	
-	_BV( 0 )	, // PG 0 ** 41 ** D41	
-	_BV( 7 )	, // PL 7 ** 42 ** D42	
-	_BV( 6 )	, // PL 6 ** 43 ** D43	
-	_BV( 5 )	, // PL 5 ** 44 ** D44	
-	_BV( 4 )	, // PL 4 ** 45 ** D45	
-	_BV( 3 )	, // PL 3 ** 46 ** D46	
-	_BV( 2 )	, // PL 2 ** 47 ** D47	
-	_BV( 1 )	, // PL 1 ** 48 ** D48	
-	_BV( 0 )	, // PL 0 ** 49 ** D49	
-	_BV( 3 )	, // PB 3 ** 50 ** SPI_MISO	
-	_BV( 2 )	, // PB 2 ** 51 ** SPI_MOSI	
-	_BV( 1 )	, // PB 1 ** 52 ** SPI_SCK	
-	_BV( 0 )	, // PB 0 ** 53 ** SPI_SS	
-	_BV( 0 )	, // PF 0 ** 54 ** A0	
-	_BV( 1 )	, // PF 1 ** 55 ** A1	
-	_BV( 2 )	, // PF 2 ** 56 ** A2	
-	_BV( 3 )	, // PF 3 ** 57 ** A3	
-	_BV( 4 )	, // PF 4 ** 58 ** A4	
-	_BV( 5 )	, // PF 5 ** 59 ** A5	
-	_BV( 6 )	, // PF 6 ** 60 ** A6	
-	_BV( 7 )	, // PF 7 ** 61 ** A7	
-	_BV( 0 )	, // PK 0 ** 62 ** A8	
-	_BV( 1 )	, // PK 1 ** 63 ** A9	
-	_BV( 2 )	, // PK 2 ** 64 ** A10	
-	_BV( 3 )	, // PK 3 ** 65 ** A11	
-	_BV( 4 )	, // PK 4 ** 66 ** A12	
-	_BV( 5 )	, // PK 5 ** 67 ** A13	
-	_BV( 6 )	, // PK 6 ** 68 ** A14	
-	_BV( 7 )	, // PK 7 ** 69 ** A15	
-};
 */
-
 long unsigned int microsecondsToInches(long unsigned int mIcroseconds) 
 {
   // According to Parallax's datasheet for the PING))), there are 73.746
@@ -596,7 +387,7 @@ unsigned long pulseIn(volatile uint8_t pInno, uint8_t vAlue)
                      	    }
                      return wIdth;
                          } break; 		//case'h':
-				  //  DDRH|(0<<c[i]);
+				  //  DDRH|(0<<c[pInno]);
 	case'e':
 	                  
 	                 { while (((PINE) && ((pInno%8))) == vAlue)
@@ -851,11 +642,7 @@ void initADC()
 	ADCSRA=(1<<ADEN)|(1<<ADPS2)|(1<<ADPS1);		//ADC enabled, Prescaler 64
 }
 
-<<<<<<< HEAD
-int analogRead(uint8_t PiNNo)
-=======
 int analogRead(int (pInno))
->>>>>>> 44189926e65d4f90ab0f5c9e92bd41f97ed954e3
 {
         //prescalar set to default
   	ADMUX=(1<<REFS0)|(0<<REFS1);
