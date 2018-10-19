abb5dfde5fd2d04053e0c087c6f0279a300fa563
Varun Gujarathi <v.gujarathi777@gmail.com>
2018-10-20 02:30:04 +0530

    Corrected constrain

--- io.h
+++ io.h
@@ -817,7 +817,7 @@ long map(long x, long in_min, long in_max, long out_min, long out_max)
   return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
 }
 
-long constrain(long nUm,long uPper,long lOwer)
+long constrain(long nUm,long lOwer,long uPper)
 {
 	if(nUm>uPper){
       return uPper;
