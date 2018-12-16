f5f965331302e04b6a0279416bcd582cbe411a5a
ParthB <41703901+ParthBasole@users.noreply.github.com>
2018-12-16 13:02:38 +0530

    Update Wire.h

--- Wire.h
+++ Wire.h
@@ -1,48 +1,34 @@
+
+
+#include <avr/io.h>
 #define  TWI_FREQ 100000L
-//#define F_CPU 1000000UL
-
-
+#define F_CPU 1000000UL
 class Wire
-{
-public:
-bool available()
-{
-if (PORTC ==3)
-{
-	return(1);
-}	
-else
-return (2);
+{ public:
 	
-}
 
 void begin()
 {DDRC|=0b00000011;
  TWBR=((F_CPU/TWI_FREQ)-16)/2;
  PORTC=(1<<0)|(1<<1);
- TWCR=(1<<TWSTA)|(1<<TWINT)|(1<<TWEN);
 }
-void begin(uint8_t sLave_address)
-{TWAR=(sLave_address||0b00000001);	
- TWBR=((F_CPU/TWI_FREQ)-16)/2;
-  TWCR=(1<<TWEN)|(1<<TWEA)|(1<<TWINT);
-}
-uint8_t requestFrom(uint8_t sLave_address)
+uint8_t requestFrom(uint8_t adress)
 {uint8_t stat;
 	DDRB=255;
+	TWCR=(1<<TWSTA)|(1<<TWINT)|(1<<TWEN);
 	while(!(TWCR&(1<<TWINT)))
 	{
-		PORTB|=7;
+		PORTB|=1;
 	}
 	stat=TWSR&0xF8;
 	if (stat==0x08)
 	{
-		PORTB=1;
+		PORTB=255;
 	}
-	TWDR=sLave_address;
+	TWDR=adress&0b11111110;
 	TWCR=(1<<TWINT)|(1<<TWEN);
 	while(!(TWCR&(1<<TWINT)));
-    stat=TWSR&0xF8;	
+    stat=TWSR&0xF8;
 	if (stat == 0x18)
 	{
 		PORTB|=16;
@@ -52,13 +38,17 @@ uint8_t requestFrom(uint8_t sLave_address)
 		PORTB|=4;
 	}
 }
-void read(uint8_t dAta)
+void write(uint8_t dta)
 { uint8_t stat;
+
+TWDR=dta;
 TWCR=(1<<TWEN)|(1<<TWINT);
+
 while(!(TWCR&(1<<TWINT)));
 if (stat==0x28)
  stat=TWSR&0xF8;
-return (TWDR);
-PORTB=8;	
+
+PORTB=8;
+
 }
 };
